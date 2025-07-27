76113e64167cd2612cad7e2dbe4b0c502d79d675
RomanEX7927 <kipropempire27@gmail.com>
2025-07-27 11:50:12 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " malvin~ZvsFVQQD#oEl9QRWGSRYn44leg-entz2NXYk8gX1U-rfjhLiSPBw",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
